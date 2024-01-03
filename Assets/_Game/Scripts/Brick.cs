22c2cdd1333bee5418fca802ce6e6317cb2aae82
Viet Anh <vietanhnguyen14to1@gmail.com>
2024-01-03 21:30:50 +0700

    add

--- Assets/_Game/Scripts/Brick.cs
+++ Assets/_Game/Scripts/Brick.cs
@@ -7,8 +7,17 @@ public class Brick : ColorObject
     private void Start()
     {
         ChangeColor((ColorType)Random.Range(2, 5));
+        Invoke("SetActive", 5.0f);
+
     }
+ 
 
-
-
+    void SetActive()
+    {
+        if (this.gameObject.activeSelf == false)
+        {
+            this.gameObject.SetActive(true);
+            ChangeColor((ColorType)Random.Range(2, 5));
+        }   
+    }
 }
