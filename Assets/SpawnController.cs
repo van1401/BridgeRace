ce16f6169e99eafa1aeb81a3eb928601696f20c4
Viet Anh <vietanhnguyen14to1@gmail.com>
2024-01-03 20:55:01 +0700

    add

--- Assets/SpawnController.cs
+++ Assets/SpawnController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
+    public List<Vector3> rootPos;
     [Range(1, 10)]
     public int width, height;
-    Vector3 origin;
     public GameObject brick;
 
     void Start()
@@ -15,10 +15,17 @@ public class SpawnController : MonoBehaviour
         {
             for (int j = -4; j < height; j++)
             {
-               
-                GameObject obj = Instantiate(brick, origin, transform.rotation, transform);
-                origin = new Vector3(transform.position.x  + (i*1.25f), 0.5f, transform.position.z + (j*1.25f));
-                obj.transform.position = origin;
+                GameObject stage = Instantiate(brick, rootPos[0], transform.rotation, transform);
+                GameObject stage1 = Instantiate(brick, rootPos[1], transform.rotation, transform);
+                GameObject stage2 = Instantiate(brick, rootPos[2], transform.rotation, transform);
+
+                Vector3 newpos = new Vector3(rootPos[0].x + (i * 1.25f), rootPos[0].y + 0.5f, rootPos[0].z + (j * 1.25f));
+                Vector3 newpos1 = new Vector3(rootPos[1].x + (i * 1.25f), rootPos[1].y + 0.5f, rootPos[1].z + (j * 1.25f));
+                Vector3 newpos2 = new Vector3(rootPos[2].x + (i * 1.25f), rootPos[2].y + 0.5f, rootPos[2].z + (j * 1.25f));
+
+                stage.transform.position = newpos;
+                stage1.transform.position = newpos1;
+                stage2.transform.position = newpos2;
             }
         }
     }
--- Assets/_Game/Scripts/Brick.cs
+++ Assets/_Game/Scripts/Brick.cs
@@ -9,4 +9,6 @@ public class Brick : ColorObject
         ChangeColor((ColorType)Random.Range(2, 5));
     }
 
+
+
 }
